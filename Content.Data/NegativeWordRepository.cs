c5bd8e9210fb28c3d134217ead3e39586f30d382
NChaganlal <nisha.chaganlal@gmail.com>
2015-01-19 20:58:40 +0000

    Story 3 - Masking Words for Reader

---
+++ Content.Data/NegativeWordRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content.Data
+{
+    public class NegativeWordRepository : INegativeWordRepository
+    {
+        private List<string> _negativeWordsList;
+
+        public List<string> NegativeWordList
+        {
+            set { _negativeWordsList = value; }
+        }
+
+        public List<string> GetNegativeWords()
+        {            
+            return _negativeWordsList;
+        }
+
+        public void SaveNegativeWords(List<string> listToCreate)
+        {
+            _negativeWordsList = listToCreate;
+        }
+    }
+}
--- ContentConsole.Test.Unit/TestDataFactory.cs
+++ ContentConsole.Test.Unit/TestDataFactory.cs
@@ -11,6 +11,54 @@ namespace ContentConsole.Test.Unit
     {
         private static readonly List<string> NegativeWords = new List<string> { "swine", "bad", "nasty", "horrible" };
 
+        public static IEnumerable MaskNegativeWordsInContentTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(NegativeWords
+                    , "I played Badminton and it was nasty."
+                    , "I played Badminton and it was n###y.")
+                    .SetName("Negative words found within a word");
+                yield return new TestCaseData(NegativeWords
+                    , "The weather in Manchester in winter is bad and nasty. It rains all the time - it must be horrible for people visiting."
+                    , "The weather in Manchester in winter is b#d and n###y. It rains all the time - it must be h######e for people visiting.")
+                    .SetName("Negative words found");
+                yield return new TestCaseData(NegativeWords
+                    , "The weather in Manchester in winter is fantastic. It is sunny all the time - it must be nice for people visiting."
+                    , "The weather in Manchester in winter is fantastic. It is sunny all the time - it must be nice for people visiting.")
+                    .SetName("There are no negative words");
+                yield return new TestCaseData(NegativeWords
+                    , null
+                    , null)
+                    .SetName("Null input is provided");
+                yield return new TestCaseData(NegativeWords
+                    , string.Empty
+                    , string.Empty)
+                    .SetName("Empty input is provided");
+                yield return new TestCaseData(NegativeWords
+                    , " "
+                    , " ")
+                    .SetName("Whitespace input is provided");
+            }
+        }
+
+        public static IEnumerable MaskNegativeWordTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(NegativeWords, "fantastic", "fantastic")
+                    .SetName("Mask non negative word");
+                yield return new TestCaseData(NegativeWords, "swine", "s###e")
+                    .SetName("Mask negative word");
+                yield return new TestCaseData(NegativeWords, null, null)
+                    .SetName("Mask null word");
+                yield return new TestCaseData(NegativeWords, string.Empty, string.Empty)
+                    .SetName("Mask empty word");
+                yield return new TestCaseData(NegativeWords, " ", " ")
+                   .SetName("Mask whitespace word");
+            }
+        }
+
         public static IEnumerable CountNegativeWordsTestCases
         {
             get
